c217803d1c16d69270791eab7658a475127b6684
Luca Paolella <luca.paolella@gmail.com>
2025-12-01 20:31:43 +0100

    cleanup day 1

--- day1/solution.py
+++ day1/solution.py
@@ -7,35 +7,28 @@ lines = input_file.read_text().splitlines()
 rotations = [int(l[1:]) * (-1 if l[0] == "L" else 1) for l in lines]
 # print(rotations)
 
+
 def part1():
     n = 50
     t = 0
     for r in rotations:
         n = (n + r) % 100
-        if n == 0:
-            t += 1
+        t += n == 0
     return t
+
 
 def part2():
     t = 0
     n = 50
+    # I'm sure there's a fancy arithmetic way to do this but I'm too tired
     for r in rotations:
-        n2 = (n + r) % 100
-        extra = 0
-        radj = r
-        if abs(r) >= 100:
-            extra = abs(r) // 100
-            radj = abs(r) % 100 * (-1 if r < 0 else 1)
-
-        s = n + radj
-        amt = s not in range(1, 100) and n != 0
-        t += abs(amt) + extra
-        if amt:
-            print(n, r, radj, amt, "ex", extra, t, "to", n2)
-        
-        n=n2
-
+        extra_amt = abs(int(r / 100))
+                   # take the remainder and keep the sign
+        base_amt = n + (r - int(r / 100) * 100) not in range(1, 100) and n != 0
+        t += base_amt + extra_amt
+        n = (n + r) % 100
     return t
+
 
 print(part1())
 print(part2())
