c1bca7e5e85ceab25ca2d02f26e5686bdd8c952f
Luca Paolella <luca.paolella@gmail.com>
2025-12-04 07:45:33 +0100

    cleanup

--- day3/solution.py
+++ day3/solution.py
@@ -4,46 +4,23 @@ current_dir = Path(__file__).parent
 input_file = Path(current_dir, "input.txt")
 
 lines = input_file.read_text().splitlines()
-maxx = len(lines[0])
-maxy = len(lines)
 
-mp = {(x, y): c for y, l in enumerate(lines) for x, c in enumerate(l)}
+points = set((x, y) for y, l in enumerate(lines) for x, _ in enumerate(l) if lines[y][x] == "@")
 
-def adj(mp, coords):
-    x, y = coords
-    return [coords for coords in [
-        (x, y + 1),
-        (x + 1, y + 1),
-        (x + 1, y),
-        (x + 1, y - 1),
-        (x, y - 1),
-        (x - 1, y - 1),
-        (x - 1, y),
-        (x - 1, y + 1),
-    ] if coords in mp]
+def get_nbs(mp, p):
+    x, y = p
+    return [nb for dx in range(-1, 2) for dy in range(-1, 2) if (dx, dy) != (0, 0) and (nb := (x + dx, y + dy)) in mp]
 
 def part1():
-    tot = 0
-    for coord in mp:
-        nbs = [c for c in adj(mp, coord) if mp[c] == "@"]
-        print(coord, mp[coord], nbs, [mp[c] for c in nbs])
-        tot += len(nbs) < 4 and mp[coord] == "@"
-    return tot
+    return sum(len(get_nbs(points, coord)) < 4 for coord in points)
 
 def part2():
-    tot = 0
-    removed_iter = 1000
-    while removed_iter > 0:
-        toremove = []
-        removed_iter = 0
-        for coord in mp:
-            nbs = [c for c in adj(mp, coord) if mp[c] == "@"]
-            if len(nbs) < 4 and mp[coord] == "@":
-                tot += 1
-                toremove.append(coord)
-                removed_iter += 1
-        for coord in toremove:
-            mp[coord] = "x"
+    tot, diff = 0, 1
+    while diff > 0:
+        prev_l = len(points)
+        points.difference_update(tuple(p for p in points if len(get_nbs(points, p)) < 4))
+        diff = prev_l - len(points)
+        tot += diff
     return tot
 
 
