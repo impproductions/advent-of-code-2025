c7c4a06d1696d0e29d94bbeecd8681f1e9287492
Luca Paolella <luca.paolella@contentsquare.com>
2025-12-05 09:40:09 +0100

    better

--- day4/solution.py
+++ day4/solution.py
@@ -5,40 +5,23 @@ input_file = Path(current_dir, "input.txt")
 
 t1, t2 = input_file.read_text().split("\n\n")
 
-ranges = [tuple(map(int, l.split("-"))) for l in t1.splitlines()]
+ranges = [list(map(int, l.split("-"))) for l in t1.splitlines()]
 ids = list(map(int, t2.splitlines()))
 
 def part1():
     return sum(any(id in range(r[0], r[1] + 1) for r in ranges) for id in ids)
 
 def part2():
-    mapped = {}
-
     ranges.sort()
-    for r in ranges:
-        # works because ranges are already sorted
-        mapped[r] = set(r2 for r2 in ranges if r2[0] in range(r[0], r[1]+1))
-
-    merged = {}
-    visited = set()
-    for r in mapped.keys():
-        if r in visited:
+    merged = ranges[:1]
+    for s2, e2 in ranges[1:]:
+        s, e = merged[-1]
+        if s2 in range(s, e+1):
+            merged[-1][1] = max(e2, e)
             continue
-        island = set([r])
-        stack = [r]
-        while len(stack) > 0:
-            curr = stack.pop()
-            nbs = mapped[curr]
-            visited.add(curr)
-            island.add(curr)
-            for n in nbs:
-                if n in island:
-                    continue
-                stack.append(n)
-        cols = list(zip(*island))
-        merged[r] = max(cols[1]) - min(cols[0]) + 1
+        merged.append([s2, max(e2, e)])
 
-    return sum(merged.values())
+    return sum(e - s + 1 for s, e in merged)
 
 
 print(part1())
