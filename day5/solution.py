5621acc2587dd809e35e8b6968964f571f9a3ba4
Luca Paolella <luca.paolella@gmail.com>
2025-12-06 09:46:08 +0100

    refactor for the lulz

--- day5/solution.py
+++ day5/solution.py
@@ -4,36 +4,16 @@ current_dir = Path(__file__).parent
 input_file = Path(current_dir, "input.txt")
 
 lines = input_file.read_text().splitlines()
-nums_s = lines[:-1]
-ops = lines[-1]
+divs = [-1] + [i for i in range(len(lines[0])) if all(l[i] == " " for l in lines)] + [len(lines[0])]
+ops = [[l[divs[i] + 1 : divs[i + 1]] for l in lines] for i in range(len(divs) - 1)]
 
-ops = []
 
-st = 0
-for i in range(len(lines[0]) + 1):
-    if i == len(lines[0]) or all(l[i] in (" ") for l in lines):
-        ops.append([l[st:i] for l in lines])
-        st = i + 1
-
-def trim_op(l: list[str]):
-    return [el.strip() for el in l]
-    
 def part1():
-    tot = 0
-    for op in ops:
-        operator = op[-1]
-        nums = op[:-1]
-        tot += eval(operator.join(nums))
-    return tot
+    return sum(eval(op[-1].join(op[:-1])) for op in ops)
+
 
 def part2():
-    tot = 0
-    for o in ops:
-        operator = o[-1]
-        nums = o[:-1]
-        nums = list(reversed(["".join(n).strip() for n in zip(*nums)]))
-        tot += eval(operator.join(nums))
-    return tot
+    return sum(eval(op[-1].join("".join(n).strip() for n in zip(*op[:-1]))) for op in ops)
 
 
 print(part1())
