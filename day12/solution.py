35dce55de2ba48e680a2ed2e0fd85ce5d35b02a4
Luca Paolella <luca.paolella@gmail.com>
2025-12-24 13:11:11 +0100

    optimization. can be improved by using sets instead of actual maps

--- day12/solution.py
+++ day12/solution.py
@@ -18,15 +18,12 @@ current_dir = Path(__file__).parent
 input_file = Path(current_dir, "input.txt")
 txt = input_file.read_text()
 
-shapes = [g[3:].splitlines() for g in txt.split("\n\n")[:6]]
+shapes = [tuple(g[3:].splitlines()) for g in txt.split("\n\n")[:6]]
 slots = [
     (tuple(l.split(" ")[0][:-1].split("x")), l.split(" ")[1:])
     for l in txt.splitlines()[30:]
 ]
 slots = [(tuple(int(n) for n in s), [int(n2) for n2 in s2]) for s, s2 in slots]
-
-pprint(slots)
-
 
 def rotate(matrix):
     m2 = [[c for c in row] for row in matrix]
@@ -34,7 +31,7 @@ def rotate(matrix):
         for x in range(len(matrix[0])):
             m2[x][y] = matrix[y][len(matrix[0]) - x - 1]
 
-    return ["".join(r) for r in m2]
+    return tuple("".join(r) for r in m2)
 
 
 def flip(matrix):
@@ -43,9 +40,9 @@ def flip(matrix):
         for x in range(len(matrix[0])):
             m2[y][x] = matrix[y][len(matrix[0]) - x - 1]
 
-    return ["".join(r) for r in m2]
+    return tuple("".join(r) for r in m2)
 
-
+@cache
 def get_all_rots(matrix):
     rot1 = matrix
     rot2 = rotate(rot1)
@@ -53,33 +50,20 @@ def get_all_rots(matrix):
     rot4 = rotate(rot3)
     allset = set(
         [
-            "".join(rot1),
-            "".join(flip(rot1)),
-            "".join(rot2),
-            "".join(flip(rot2)),
-            "".join(rot3),
-            "".join(flip(rot3)),
-            "".join(rot4),
-            "".join(flip(rot4)),
+            rot1,
+            flip(rot1),
+            rot2,
+            flip(rot2),
+            rot3,
+            flip(rot3),
+            rot4,
+            flip(rot4),
         ]
     )
-    return list(allset)
+    return allset
 
 
-def rtom(r):
-    return [r[:3], r[3:6], r[6:]]
-
-
-
-def part2():
-    return
-
-
-# print(part1())
-# print(part2())
-
-
-def grow(m1, position, m2, letter="#"):
+def grow(m1, position, m2):
     xpos, ypos = position
     w1, h1 = len(m1[0]), len(m1)
     w2, h2 = len(m2[0]), len(m2)
@@ -87,8 +71,6 @@ def grow(m1, position, m2, letter="#"):
     w = max(w1, xpos + w2)
     h = max(h1, ypos + h2)
     new_m = [["." for x in range(w)] for y in range(h)]
-    # print(xpos, ypos)
-    # print(w1, h1, w2, h2, w, h)
     for y in range(h1):
         for x in range(w1):
             new_m[y][x] = m1[y][x]
@@ -103,28 +85,21 @@ def grow(m1, position, m2, letter="#"):
                 new_m[y][x] = "O"
                 return None
             else:
-                new_m[y][x] = letter
+                new_m[y][x] = "#"
 
     return tuple(tuple(r) for r in new_m)
-
-letters = "abcdefghijklmnpqrstuvwxyz1234567890"
-letters = letters + letters.upper()
 
 from functools import cache
 
 @cache
-def count_to_place(m, invert=False):
+def count_to_place(m):
     t = 0
     for y in range(len(m)):
         for x in range(len(m[0])):
-            if not invert:
-                t += m[y][x] != "."
-            else:
-                t += m[y][x] == "."
+            t += m[y][x] != "."
     return t
 
-@cache
-def grow_all(current, amts, limit, depth=0):
+def grow_all(current, amts, limit, not_fit):
     maxw, maxh = limit
     if len(current) > maxh or len(current[0]) > maxw:
         return None
@@ -140,14 +115,12 @@ def grow_all(current, amts, limit, depth=0):
     to_place = sum(count_to_place(tuple(shapes[i])) * amt for i, amt in enumerate(amts))
     room = limit[0] * limit[1] - count_to_place(current)
 
-    # print("room", room, to_place)
     if room < to_place:
         return None
 
-    # print("new cycle", depth, "remaining", amts)
     for i, shp_amt in enumerate(options):
         shape = shapes[i]
-        opts = [rtom(r) for r in get_all_rots(shape)]
+        opts = get_all_rots(shape)
         if shp_amt == 0:
             continue
 
@@ -155,21 +128,16 @@ def grow_all(current, amts, limit, depth=0):
             # print("------")
             for y in range(len(current)):
                 for x in range(len(current[0])):
-                    new_m = grow(current, (x, y), opt, letters[depth % len(letters)])
+                    if (x, y) in not_fit:
+                        continue
+                    new_m = grow(current, (x, y), opt)
+                    if not new_m:
+                        not_fit.add((x, y))
                     if new_m:
-                        # print("trying", (len(current[0]), len(current)), shape, shp_amt)
-                        # print(shp(current))
-                        # print("vs")
-                        # print(shp(opt))
-                        # print("grown to")
-                        # print(shp(new_m))
-
-                        new_options = list(options)
+                        new_options = options.copy()
                         new_options[i] -= 1
-                        new_options = tuple(new_options)
 
-                        # print("re-calling with", new_m, new_opts, limit)
-                        res = grow_all(new_m, new_options, limit, depth + 1)
+                        res = grow_all(new_m, new_options, limit, set(list(not_fit)))
                         if res is not None:
                             return res
 
@@ -184,12 +152,13 @@ def part1():
 def check_slot(args):
     idx, slot, amts = args
     for i, amt in enumerate(amts):
+        print("doing", amts, i, slot)
         if amt == 0:
             continue
-        r1 = tuple(tuple(r) for r in shapes[i])
+        r1 = shapes[i]
         amts2 = list(amts)
         amts2[i] -= 1
-        res = grow_all(r1, tuple(amts2), slot)
+        res = grow_all(r1, amts2, slot, set())
         if res is not None:
             return True
     return False
@@ -206,15 +175,6 @@ def part1_parallel(parallel=True, workers=None, total_expected=1000):
             idx = next(cnt)
             print("trying slot", idx, slot, amts)
             # ETA calculation
-            elapsed = time.time() - start_time
-            cycles_done = idx + 1
-            avg_per_cycle = elapsed / cycles_done if cycles_done > 0 else 0
-            remaining_cycles = max(total_expected - cycles_done, 0)
-            eta_secs = remaining_cycles * avg_per_cycle
-            eta_min = int(eta_secs // 60)
-            eta_sec = int(eta_secs % 60)
-            print(f"elapsed={elapsed:.1f}s, avg={avg_per_cycle:.3f}s/cycle, ETA={eta_min}m{eta_sec}s (for {remaining_cycles} cycles)")
-
             for i, amt in enumerate(amts):
                 if amt == 0:
                     continue
@@ -223,7 +183,7 @@ def part1_parallel(parallel=True, workers=None, total_expected=1000):
 
                 print("- start from", amts2, r1)
                 amts2[i] -= 1
-                res = grow_all(r1, tuple(amts2), slot)
+                res = grow_all(r1, amts2, slot, set())
                 if res is not None:
                     has = True
                     break
@@ -234,7 +194,7 @@ def part1_parallel(parallel=True, workers=None, total_expected=1000):
         return fitting
 
     # parallel execution using multiprocessing Pool
-    workers = workers or cpu_count()
+    workers = workers or cpu_count() - 1
     with Pool(workers) as pool:
         args_iter = ((idx, slot, amts) for idx, (slot, amts) in enumerate(slots))
         processed = 0
@@ -258,10 +218,6 @@ if __name__ == "__main__":
     result = part1_parallel(parallel=True)
     print(result)
 
-
-r1 = shapes[0]
-r2 = shapes[1]
-r3 = shapes[2]
 
 # print(shp(r1))
 # print(shp(r2))
